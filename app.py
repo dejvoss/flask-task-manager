631f7634d89a402dd740b7efe254dce00dbd06b0
dejvoss <deosiecki@gmail.com>
2020-06-01 09:04:40 +0000

    Add gitignore and env.py link, add template tasks.html, add code to display tasks, add connection to mongodb, add funvtion get tasks to retrieve tasks template, import from flask render_template, redirect, request, url for, import pymongo

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+if os.path.exist("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.rout('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
-    app.run(host = os.environ.get('IP'),
-            port= int(os.environ.get('PORT')),
-            debug = True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
