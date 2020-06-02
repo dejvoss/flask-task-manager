cebce6f20097ccd80a912af02e2f573c06d3c33a
dejvoss <deosiecki@gmail.com>
2020-06-02 20:05:10 +0000

    Add new template for adding tasks, add links to the add task web and to go back from it

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template('add_task.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
