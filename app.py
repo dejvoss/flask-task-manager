4e10793f4a31be7dc21a4ccb3f0856cccd9ddb57
dejvoss <deosiecki@gmail.com>
2020-06-06 12:34:01 +0200

    fix the bug with edit task - tasks instead of task; fix the bug with calendar - replace js link to materialize for newest

--- app.py
+++ app.py
@@ -29,6 +29,14 @@ def insert_task():
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+                           
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
