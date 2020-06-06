fb880a0b78094cabd670985f8391df1ff7fe8b69
dejvoss <deosiecki@gmail.com>
2020-06-06 10:51:51 +0200

    Add submit button to adding task webpage and add functionality to ading task to the database

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ def get_tasks():
 def add_task():
     return render_template('add_task.html', categories=mongo.db.categories.find())
 
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
