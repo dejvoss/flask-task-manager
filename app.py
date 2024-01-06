d756682cf8d80e5d91248128968e5f65d9c2e479
dejvoss <deosiecki@gmail.com>
2024-01-06 18:24:23 +0100

    Upgrade application dependencies; fix form issue

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config[
+    "MONGO_URI"] = 'mongodb+srv://kismvoqp:6ALp9UOaj9EVx5FGhc1I@myfirstcluster.yipty.mongodb.net/task_manager'
+# app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
+print(mongo.db)
 
 
 @app.route('/')
@@ -19,9 +22,11 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+
 @app.route('/add_task')
 def add_task():
     return render_template('add_task.html', categories=mongo.db.categories.find())
+
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
@@ -29,58 +34,67 @@ def insert_task():
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
+
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    all_categories =  mongo.db.categories.find()
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task,
                            categories=all_categories)
+
 
 @app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
-    tasks.update( {'_id': ObjectId(task_id)},
-    {
-        'task_name':request.form.get('task_name'),
-        'category_name':request.form.get('category_name'),
-        'task_description': request.form.get('task_description'),
-        'due_date': request.form.get('due_date'),
-        'is_urgent':request.form.get('is_urgent')
-    })
-    return redirect(url_for('get_tasks'))                
+    tasks.update({'_id': ObjectId(task_id)},
+                 {
+                     'task_name': request.form.get('task_name'),
+                     'category_name': request.form.get('category_name'),
+                     'task_description': request.form.get('task_description'),
+                     'due_date': request.form.get('due_date'),
+                     'is_urgent': request.form.get('is_urgent')
+                 })
+    return redirect(url_for('get_tasks'))
+
 
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
-    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    mongo.db.tasks.delete_one({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
+
 
 @app.route('/get_categories')
 def get_categories():
-    return render_template('categories.html', 
-    categories=mongo.db.categories.find())
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
+
 
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    mongo.db.categories.delete_one({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
+
 
 @app.route('/add_category')
 def add_category():
     return render_template('add_category.html')
 
+
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
-    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+                           category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
 
 @app.route('/update_category/<category_id>', methods=["POST"])
 def update_category(category_id):
     categories = mongo.db.categories
-    categories.update( {'_id': ObjectId(category_id)},
-    {
-        'category_name':request.form.get('category_name'),    
-    })
-    return redirect(url_for('get_categories'))   
+    categories.update({'_id': ObjectId(category_id)},
+                      {
+                          'category_name': request.form.get('category_name'),
+                      })
+    return redirect(url_for('get_categories'))
+
 
 @app.route('/insert_category', methods=["POST"])
 def insert_category():
@@ -88,7 +102,9 @@ def insert_category():
     categories.insert_one(request.form.to_dict())
     return redirect(url_for('get_categories'))
 
+
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+    app.run(debug=True)
+    # app.run(host=os.environ.get('IP'),
+    #         port=int(os.environ.get('PORT')),
+    #         debug=True)
